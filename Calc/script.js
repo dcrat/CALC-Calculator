4968bdfdbab1aee89ca16f6fbbb7636095b95d87
dcrat <134829260+dcrat@users.noreply.github.com>
2023-05-31 23:25:28 +0530

    Add files via upload

---
+++ Calc/script.js
@@ -0,0 +1,12 @@
+console.log(" WELCOME TO_CALC-Your Calculations")
+function clearScreen() {
+    document.getElementById("result").value = "";
+}
+ function display(value) {
+    document.getElementById("result").value += value;
+}
+ function calculate() {
+    var p = document.getElementById("result").value;
+    var q = eval(p);
+    document.getElementById("result").value = q;
+}
